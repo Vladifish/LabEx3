ac333df25a8caf9c64a55964375963b657ab47cb
Prog.Ence04 <74560055+enta4prog@users.noreply.github.com>
2023-03-06 14:54:48 +0800

    --added the inputs for t, s, and n.

--- LabEx3.java
+++ LabEx3.java
@@ -15,6 +15,21 @@ public class LabEx3 {
         System.out.println("Velazco, Vladimir Gray");
         System.out.println(" ");
 
+        System.out.println("Enter a non-negative integer:");
+        int t = input.nextInt();
+
+        System.out.print("Enter the number of syllables in the song (s): ");
+        int s = input.nextInt();
+
+        System.out.print("Enter the number of children (up to 26) (n): ");
+        int n = input.nextInt();
+
+
+
+
+
+
+
 
 
     }
